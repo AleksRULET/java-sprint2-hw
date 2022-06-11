8c3e28a3bd0a7a662227ccc2950f837a60d5508e
Aleks_Rulet <ananim-kaka@yandex.ru>
2022-06-11 11:48:42 +0300

    fix: Исправить замечания

--- test/managers/InMemoryHistoryManagerTest.java
+++ test/managers/InMemoryHistoryManagerTest.java
@@ -3,6 +3,7 @@ package managers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import tasks.Epic;
+import tasks.Status;
 import tasks.Subtask;
 import tasks.Task;
 
@@ -17,18 +18,13 @@ class InMemoryHistoryManagerTest {
     Task task;
     Epic epic;
     Subtask subtask;
-    TaskManager taskManager;
 
     @BeforeEach
     void init() {
         historyManager = new InMemoryHistoryManager();
-        task = new Task("Задача", "Описание", 10, LocalDateTime.of(2022, 5, 15, 19, 30));
-        epic = new Epic("Эпик", "Описание");
-        subtask = new Subtask("Подзадача", "Описание", 10, LocalDateTime.of(2022, 6, 15, 19, 30), epic);
-        taskManager = new InMemoryTaskManager(historyManager);
-        taskManager.createTask(task);
-        taskManager.createTask(epic);
-        taskManager.createTask(subtask);
+        task = new Task(1, "Описание", "Задача", Status.NEW, 10, LocalDateTime.of(2022, 5, 15, 19, 30), LocalDateTime.of(2022, 5, 15, 19, 40));
+        epic = new Epic(2, "Описание", "Эпик", Status.NEW, 10, LocalDateTime.of(2022, 6, 15, 19, 30), LocalDateTime.of(2022, 6, 15, 19, 40));
+        subtask = new Subtask(3,"Описание", "Подзадача", Status.NEW, 10, LocalDateTime.of(2022, 6, 15, 19, 30), LocalDateTime.of(2022, 6, 15, 19, 40), epic.getID());
     }
 
     @Test
